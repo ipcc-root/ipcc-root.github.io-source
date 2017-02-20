e3c96eff704696d078a352890fd6c17ba2bd2c98
Peter Elmer <Peter.Elmer@cern.ch>
2017-02-20 19:03:00 +0100

    Remove liquid* plugins, not needed

--- pelicanconf.py
+++ pelicanconf.py
@@ -86,10 +86,7 @@ NOTEBOOK_DIR = 'downloads/notebooks'
 FAVICON= "images/favicon.ico"
 
 PLUGIN_PATHS = ['pelican-plugins']
-PLUGINS = ['summary', 'liquid_tags.img', 'liquid_tags.video',
-			'liquid_tags.youtube', 'render_math',
-           'liquid_tags.include_code', 'liquid_tags.notebook',
-           'liquid_tags.literal', 'tipue_search']
+PLUGINS = ['summary', 'tipue_search']
 
 
 # comments
